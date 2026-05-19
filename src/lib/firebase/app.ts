5688b42af70c0a27f360c7b3bd73d636da3618ff
jonathanferreras <jonathanferreras@icloud.com>
2026-05-19 15:38:34 -0400

    feat(lib-firebase): extract app init to separate file

---
+++ src/lib/firebase/app.ts
@@ -0,0 +1,46 @@
+import {
+  applicationDefault,
+  cert,
+  getApp,
+  getApps,
+  initializeApp,
+  ServiceAccount,
+} from "firebase-admin/app";
+
+const getServiceAccount = (): ServiceAccount | null => {
+  const projectId = process.env.FIREBASE_PROJECT_ID;
+  const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
+  const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n");
+
+  if (!projectId || !clientEmail || !privateKey) {
+    return null;
+  }
+
+  return {
+    projectId,
+    clientEmail,
+    privateKey,
+  };
+};
+
+const initializeFirebaseAdmin = () => {
+  if (getApps().length > 0) {
+    return getApp();
+  }
+
+  const serviceAccount = getServiceAccount();
+
+  if (serviceAccount) {
+    return initializeApp({
+      credential: cert(serviceAccount),
+      projectId: serviceAccount.projectId,
+    });
+  }
+
+  return initializeApp({
+    credential: applicationDefault(),
+    projectId: process.env.FIREBASE_PROJECT_ID,
+  });
+};
+
+export { initializeFirebaseAdmin };
--- src/lib/firebase/db.ts
+++ src/lib/firebase/db.ts
@@ -1,48 +1,5 @@
-import {
-  applicationDefault,
-  cert,
-  getApp,
-  getApps,
-  initializeApp,
-  ServiceAccount,
-} from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-
-const getServiceAccount = (): ServiceAccount | null => {
-  const projectId = process.env.FIREBASE_PROJECT_ID;
-  const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
-  const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n");
-
-  if (!projectId || !clientEmail || !privateKey) {
-    return null;
-  }
-
-  return {
-    projectId,
-    clientEmail,
-    privateKey,
-  };
-};
-
-const initializeFirebaseAdmin = () => {
-  if (getApps().length > 0) {
-    return getApp();
-  }
-
-  const serviceAccount = getServiceAccount();
-
-  if (serviceAccount) {
-    return initializeApp({
-      credential: cert(serviceAccount),
-      projectId: serviceAccount.projectId,
-    });
-  }
-
-  return initializeApp({
-    credential: applicationDefault(),
-    projectId: process.env.FIREBASE_PROJECT_ID,
-  });
-};
+import { initializeFirebaseAdmin } from "./app";
 
 const app = initializeFirebaseAdmin();
 const db = getFirestore(app);
