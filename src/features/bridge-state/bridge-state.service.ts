24c27aa850018b6bbe09b9c497b83ad1382b8ee6
jonathanferreras <jonathanferreras@icloud.com>
2026-05-29 20:42:21 -0400

    feat(bridge-state): add new update current bridge state implementation

--- src/features/bridge-state/bridge-state.service.ts
+++ src/features/bridge-state/bridge-state.service.ts
@@ -1,11 +1,11 @@
-import { doc, getDoc, onSnapshot } from "firebase/firestore";
+import { collection, doc, getDoc, onSnapshot, serverTimestamp, writeBatch } from "firebase/firestore";
 
 import { db } from "@/src/lib/firebase/db";
 import {
   BRIDGE_STATE_COLLECTION,
   EVENTS_COLLECTION,
 } from "@/src/lib/firebase/collections";
-import { CURRENT_BRIDGE_STATE_ID } from "./bridge-state.types";
+import { CURRENT_BRIDGE_STATE_ID, CurrentBridgeStatePayload } from "./bridge-state.types";
 import { logError } from "@/src/lib/errors";
 
 const getCurrentBridgeState = async () => {
@@ -37,7 +37,11 @@ const subscribeToBridgeState = (
         callback(null);
       } else {
         const data = docSnap.data()
-        callback({ ...data, updatedAt: data.updatedAt.toDate().toDateString() });
+        const updatedAt = data.updatedAt
+          ? data.updatedAt.toDate().toDateString()
+          : null
+
+        callback({ ...data, updatedAt });
       }
     },
     (error) => {
@@ -46,14 +50,46 @@ const subscribeToBridgeState = (
   );
 };
 
+const updateCurrentBridgeState = async (update: CurrentBridgeStatePayload) => {
+  try {
+    const batch = writeBatch(db);
+    const currentBridgeStateRef = doc(db, BRIDGE_STATE_COLLECTION, CURRENT_BRIDGE_STATE_ID);
+    const eventsRef = doc(collection(db, EVENTS_COLLECTION));
+
+    batch.update(currentBridgeStateRef, {
+      ...update,
+      id: CURRENT_BRIDGE_STATE_ID,
+      updatedAt: serverTimestamp()
+    });
+
+    batch.set(eventsRef, {
+      id: eventsRef.id,
+      sourceId: update.sourceId,
+      sourceType: update.sourceType,
+
+      position: update.position,
+      positionConfidence: update.positionConfidence,
+
+      traffic: update.traffic,
+      trafficConfidence: update.trafficConfidence,
+
+      occurredAt: serverTimestamp(),
+      createdAt: serverTimestamp(),
+    });
+
+    await batch.commit();
+  } catch (error) {
+    logError("Bridge State Service", "Failed to authenticate user.", error);
+    throw error;
+  }
+};
+
 // TODO: Implement with firebase client sdk
 const addBridgeStateEvent = async (newEvent: any) => { };
-
-const updateCurrentBridgeState = async (update: any) => { };
-
 const toggleAcceptsDeviceUpdates = async (acceptsDeviceUpdates: boolean) => { };
 
 export const bridgeStateService = {
   getCurrentBridgeState,
-  subscribeToBridgeState
+  subscribeToBridgeState,
+  updateCurrentBridgeState
 };
