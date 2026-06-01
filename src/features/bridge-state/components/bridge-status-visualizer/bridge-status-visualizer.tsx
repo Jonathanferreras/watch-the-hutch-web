66d1fabd31b26ba48203a631142ce20d1c9537e1
jonathanferreras <jonathanferreras@icloud.com>
2026-05-31 20:48:26 -0400

    build(refactor): update bridge status visualizer to include enums of bridge positions

--- src/features/bridge-state/components/bridge-status-visualizer/bridge-status-visualizer.tsx
+++ src/features/bridge-state/components/bridge-status-visualizer/bridge-status-visualizer.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { CurrentBridgeState } from "../../bridge-state.types";
+import { BRIDGE_POSITION, CurrentBridgeState } from "../../bridge-state.types";
 import { BridgeTrafficFlowScene } from "./bridge-traffic-flow-scene";
 import { BridgeTransitionScene } from "./bridge-transition-scene";
 import { BridgeOpenScene } from "./bridge-open-scene";
@@ -9,11 +9,11 @@ import { useBridgeState } from "../../hooks/use-bridge-state";
 export function BridgeStatusVisualizer({ state }: { state: CurrentBridgeState }) {
     const { data } = useBridgeState();
 
-    if (state.position === "closed" && data?.traffic) {
+    if (state.position === BRIDGE_POSITION.CLOSED && data?.traffic) {
         return <BridgeTrafficFlowScene traffic={data.traffic} />
-    } else if (state.position === "opening" || state.position === "closing") {
+    } else if (state.position === BRIDGE_POSITION.OPENING || state.position === BRIDGE_POSITION.CLOSING) {
         return <BridgeTransitionScene />
-    } else if (state.position === "open") {
+    } else if (state.position === BRIDGE_POSITION.OPEN) {
         return <BridgeOpenScene />
     } else {
         return (<></>);
